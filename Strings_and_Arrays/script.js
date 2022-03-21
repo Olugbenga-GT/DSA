d9063ba568625d38120a72b9bfd162583fa4e20b
Gastt <olugbengagt@gmail.com>
2022-03-21 01:08:56 +0100

    Strings Methods refreshehd

---
+++ Strings_and_Arrays/script.js
@@ -0,0 +1,7 @@
+let gastt= 'Alao Oluwagbemiga Timothy'
+let nickName = 'Gastt'
+
+// console.log(nickName.concat('My Nigga').repeat(7));
+// console.log(nickName.replaceAll('t', 'i'))
+// The .at and charAt method work exactly the same.
+console.log(nickName.split(','))
